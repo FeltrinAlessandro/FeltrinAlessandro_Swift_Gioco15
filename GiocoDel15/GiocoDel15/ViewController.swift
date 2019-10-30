ddd5005f3edaa8986bcc91bd58f638e4d1889cf9
Unknown <alessandro.feltrin@barsanti.edu.it>
2019-10-30 12:39:39 +0100

    30/10/2019-inizio

---
+++ GiocoDel15/GiocoDel15/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GiocoDel15
+//
+//  Created by ALESSANDRO FELTRIN on 30/10/2019.
+//  Copyright © 2019 ALESSANDRO FELTRIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var matrice: [[int]] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func ordinaMatrice() {
+        
+    }
+
+
+}
+
