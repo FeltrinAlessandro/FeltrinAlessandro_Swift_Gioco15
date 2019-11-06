6c2e2cd2e384ec2f5f664519b9ce33938a25f767
Unknown <alessandro.feltrin@barsanti.edu.it>
2019-11-06 12:36:30 +0100

    6/10/2019-metodo ordinaMatrice() e aggiunta array di bottoni

--- GiocoDel15/GiocoDel15/ViewController.swift
+++ GiocoDel15/GiocoDel15/ViewController.swift
@@ -10,15 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var btn_Mescola: UIButton!
+    @IBOutlet var gruppoBtn_All: [UIButton]!
+    
     var matrice :[[Int]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ordinaMatrice()
     }
     
-    func ordinaMatrice()  {
+    @IBAction func mischiaMatrice(_ sender: Any) {
+    
+    }
+    
+    func applicaMatriceMischiata() {
         
+    }
+    
+    
+    func ordinaMatrice()  {
+        var x = 0
+        var y = 0
+         gruppoBtn_All[15].setTitle(" ", for:.normal)
+        for i in 0...14{
+        gruppoBtn_All[i].setTitle(String(i+1), for:.normal)
+        }
+
+        
+        for i in 0...15{
+            if(x==3 && y==3)
+            {
+                matrice[x][y] = 0
+            }
+            else if(x==3 && y<3)
+            {
+                matrice[x][y] = i
+                x = 0
+                y+=1
+            }
+            else
+            {
+                matrice[x][y] = i
+                x+=1
+            }
+        }
     }
 
 
